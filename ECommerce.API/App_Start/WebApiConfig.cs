9c285a41fafc305643f5bc8871e6db0c5fb1a857
Abdul Baset Shahid <abdul.baset@atomapgroup.com>
2017-09-07 09:45:39 +0600

    First stable commit of first art

--- ECommerce.API/App_Start/WebApiConfig.cs
+++ ECommerce.API/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using Newtonsoft.Json.Serialization;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
 
 namespace ECommerce.API
@@ -19,6 +20,9 @@ namespace ECommerce.API
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
---
+++ ECommerce.API/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+using ECommerce.API.Providers;
+
+[assembly: OwinStartup(typeof(ECommerce.API.Startup))]
+
+namespace ECommerce.API
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureOAuth(app);
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+
+        private void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions oAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+            app.UseOAuthAuthorizationServer(oAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+    }
+}
